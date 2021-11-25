5dccf655649e0f674386e61fe2c3f919e6718ea1
Jordan Needham <jdnCreations@hotmail.com>
2021-11-25 13:07:12 +0800

    initial commit

---
+++ src/data.js
@@ -0,0 +1,24 @@
+const data = {
+    products: [
+        {
+            id: '1',
+            name: 'One-on-one',
+            price: '20',
+            image: 'https://images.unsplash.com/photo-1637740000677-12a6d7ddc801?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80'
+        },
+        {
+            id: '2',
+            name: 'Assessment',
+            price: '10',
+            image: 'https://images.unsplash.com/photo-1637740000677-12a6d7ddc801?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80'
+        },
+        {
+            id: '3',
+            name: 'Group Session',
+            price: '100',
+            image: 'https://images.unsplash.com/photo-1637740000677-12a6d7ddc801?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80'
+        },
+    ]
+}
+
+export default data;
